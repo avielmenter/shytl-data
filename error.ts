2627c1a682b44416d26eda2261c5127c595d6aba
Aviel Menter <aviel.menter@gmail.com>
2023-06-19 19:00:46 -0700

    made Error an abstract interface

--- error.ts
+++ error.ts
@@ -1,30 +1,31 @@
-export type ParseError = {
-    type: "Error",
-    errorType: "ParseError",
-    error: any
-}
-
-export type ServerError = {
-    type: "Error",
-    errorType: "ServerError",
-    error: any
-}
-
-export type UpdateError = {
-    type: "Error",
-    errorType: "UpdateError",
-    error: any
-}
-
 export type ErrorTypes 
     = "ParseError" 
     | "ServerError" 
     | "UpdateError";
 
-export type Error 
-    = ParseError
-    | ServerError
-    | UpdateError;
+export interface Error {
+    type: "Error",
+    errorType: ErrorTypes,
+    error: any
+}
+
+export interface ParseError extends Error {
+    type: "Error",
+    errorType: "ParseError",
+    error: any
+}
+
+export interface ServerError extends Error {
+    type: "Error",
+    errorType: "ServerError",
+    error: any
+}
+
+export interface UpdateError extends Error {
+    type: "Error",
+    errorType: "UpdateError",
+    error: any
+}
 
 export function err<T extends Error>(errorType: ErrorTypes, error: any): T {
     return {
